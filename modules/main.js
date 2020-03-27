7a5714a42629891504015d2d5d93e3f457406aea
Trevor D. Miller <5497885+trevordmiller@users.noreply.github.com>
2020-03-27 16:53:04 -0600

    Get reddit rss rendering

--- modules/main.js
+++ modules/main.js
@@ -1,57 +1,66 @@
+import titleDataToTitle from "./titleDataToTitle/titleDataToTitle.js";
 import descriptionDataToSummary from "./descriptionDataToSummary/descriptionDataToSummary.js";
 import feedDataToHtml from "./feedDataToHtml/feedDataToHtml.js";
 
 const main = async () => {
-  window.document.body.innerHTML = "<main><p>Loading...</p></main>";
+  try {
+    window.document.body.innerHTML = "<main><p>Loading...</p></main>";
 
-  const feeds = [
-    {
-      title: "IEEE",
-      url: "https://csdl-api.computer.org/api/rss/periodicals/mags/so/rss.xml"
-    },
-    {
-      title: "GitHub Trends",
-      url: "https://mshibanami.github.io/GitHubTrendingRSS/monthly/all.xml"
-    }
-  ];
+    // Direct or via https://rss.app
+    const feeds = [
+      {
+        title: "IEEE",
+        url: "https://csdl-api.computer.org/api/rss/periodicals/mags/so/rss.xml",
+        max: 3
+      },
+      {
+        title: "GitHub Trends",
+        url: "https://mshibanami.github.io/GitHubTrendingRSS/monthly/all.xml",
+        max: 10
+      },
+      {
+        title: "Reddit programming",
+        url: "https://rss.app/feeds/qtY5lEd1YADUScA0.xml",
+        max: 10
+      },
+    ];
 
-  const max = 5;
+    const parseFeeds = async feeds =>
+      await Promise.all(
+        feeds.map(async feed => {
+          const response = await window.fetch(feed.url);
+          const text = await response.text();
+          const parsedXml = new window.DOMParser().parseFromString(
+            text,
+            "text/xml"
+          );
+          const xmlItems = Array.from(parsedXml.querySelectorAll("item"));
 
-  const parseFeeds = async feeds =>
-    await Promise.all(
-      feeds.map(async feed => {
-        const response = await window.fetch(feed.url);
-        const text = await response.text();
-        const parsedXml = new window.DOMParser().parseFromString(
-          text,
-          "text/xml"
-        );
-        const xmlItems = Array.from(parsedXml.querySelectorAll("item"));
+          return {
+            title: feed.title,
+            items: xmlItems.slice(0, feed.max).map(xmlItem => {
+              const title = titleDataToTitle(xmlItem.querySelector("title").innerHTML);
+              const link = xmlItem.querySelector("link").innerHTML;
+              const descriptionData = xmlItem.querySelector("description")
+                .innerHTML;
+              const summary = descriptionDataToSummary(descriptionData);
 
-        return {
-          title: feed.title,
-          items: xmlItems.slice(0, max).map(xmlItem => {
-            const title = xmlItem.querySelector("title").innerHTML;
-            const link = xmlItem.querySelector("link").innerHTML;
-            const descriptionData = xmlItem.querySelector("description")
-              .innerHTML;
-            const summary = descriptionDataToSummary(descriptionData);
+              return {
+                title: title,
+                link: link,
+                summary: summary
+              };
+            })
+          };
+        })
+      );
 
-            console.log(descriptionData);
+    const parsedFeeds = await parseFeeds(feeds);
 
-            return {
-              title: title,
-              link: link,
-              summary: summary
-            };
-          })
-        };
-      })
-    );
-
-  const parsedFeeds = await parseFeeds(feeds);
-
-  window.document.body.innerHTML = feedDataToHtml(parsedFeeds);
+    window.document.body.innerHTML = feedDataToHtml(parsedFeeds);
+  } catch (error) {
+    window.document.body.innerHTML = "<main><p>There was an error.</p></main>";
+  }
 };
 
 main();
---
+++ modules/titleDataToTitle/titleDataToTitle.js
@@ -0,0 +1,9 @@
+const titleDataToTitle = titleData => {
+  const textContent = titleData
+    .replace("<![CDATA[", "")
+    .replace("]]>", "");
+
+  return textContent;
+};
+
+export default titleDataToTitle;
